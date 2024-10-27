a213160aea92c986d0d07449c3ce4125df92102b
Tong Xu <erichilla0525@gmail.com>
2024-10-27 03:41:55 -0500

    ADDing documentation to strategy file

--- patterns/strategy/minimum_balance_strategy.py
+++ patterns/strategy/minimum_balance_strategy.py
@@ -1,6 +1,6 @@
 """
-Description: {description of the file.}
-Author: {student name}
+Description: {MinimumBalanceStrategy class}
+Author: {Tong Xu}
 """
 
 from patterns.strategy.service_charge_strategy import ServiceChargeStrategy
@@ -8,16 +8,32 @@ from bank_account.bank_account import BankAccount
 
 class MinimumBalanceStrategy(ServiceChargeStrategy):
     """
+    A class for the minimum balance strategy.
+    Attributes:
+        SERVICE_CHARGE_PREMIUM(float): A mutiplier that applied to the base_service_charg if account balance less than the minimum balance.
+        minimum_balance(float): The amount of minimum balance that does not need to apply the SERVICE_CHARGE_PREMIUM.
+
+    Methods:
+        calculate_service_charges(account: BankAccount): Calculate the service charge based on the account balance.
+
     """
     SERVICE_CHARGE_PREMIUM = 2.0
 
     def __init__(self, minimum_balance: float):
         """
+        Initiazlize an instance of minimum balance strategy with given minimum balance of the account.
+        Args:
+            minimum_balance(float): Minimum balance of the account that will avoid to apply the SERVICE_CHARGE_PREMIUM multiplier.
         """
         self.__minimum_balance = minimum_balance
 
     def calculate_service_charges(self, account: BankAccount) -> float:
         """
+        Calculate the service charge based on the balance of given account.
+        Args:
+            account: BankAccount: The bank account that applie the calculation of service charge.
+        Return:
+            A float that represent the result of service charge calculation.
         """
         if account.balance >= self.__minimum_balance:
             return self.Base_Service_Charge
--- patterns/strategy/service_charge_strategy.py
+++ patterns/strategy/service_charge_strategy.py
@@ -8,11 +8,24 @@ from bank_account.bank_account import BankAccount
 
 class ServiceChargeStrategy(ABC):
     """
+    Abstract base class for service charge strategies applied to bank accounts.
+    Attributes:
+        Base_Service_Charge(float): The base service charge that applies the bank accounts.
+    Methods:
+        calculate_service_chargs(account: BankAccount): Calculate service charge for the given account.
+
+
     """
     Base_Service_Charge = 0.50
     
     @abstractmethod
     def calculate_service_charges(self, account: BankAccount) -> float:
         """
+        Calculate service charge for the given account.
+        Args:
+            account(BankAccount): The account for that service charge been calculated.
+        Return:
+            A float that represent the result of service charge calculation.
+
         """
         pass
