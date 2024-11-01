72427789f28b5988e129ca0c07248e0295c855a1
Tong Xu <erichilla0525@gmail.com>
2024-10-31 22:02:54 -0500

    Retry committing the observer files

--- patterns/observer/observer.py
+++ patterns/observer/observer.py
@@ -6,9 +6,16 @@ from abc import ABC, abstractmethod
 
 class Observer(ABC):
     """
+    Abstracted base class for observer pattern
+    Method:
+        update(message: str): Update the message received from subjects.
     """
     @abstractmethod
     def update(self, message: str):
         """
+        Update the message received from the subject.
+
+        Args:
+            meessage(str): Message from the subject.
         """
         pass
